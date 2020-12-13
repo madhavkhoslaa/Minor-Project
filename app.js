ea00727cc59d7ae6d80679638557da607915875a
Madhav Khosla <madhavkhosla@Madhavs-MacBook-Air.local>
2020-12-13 14:51:46 +0530

    cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Coordinates = require('./routes/query')
-
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(Coordinates)
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 3000, () => {
     
 })
