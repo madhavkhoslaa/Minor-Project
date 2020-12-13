bec8a8ef8d9f97cd2f01d588ef749711ecc7d4cb
Madhav Khosla <madhavkhosla@Madhavs-MacBook-Air.local>
2020-12-13 07:10:10 +0530

    routes

---
+++ routes/query.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cood = require("../utils/co-ordinates")
+const generation = require("../utils/generation")
+
+const query = express.Router()
+
+query.get(('/pointsdedo/'), (req, res) => {
+    const bounds = cood.bounds(req.body.cood)
+    var bounds_ = []
+    bounds.forEach((latlong) => {
+        bounds_.push(latlong[0])
+    })
+    const mesh = generation.generate(bounds_, req.body.distance)
+    res.send(mesh)
+})
+
+module.exports = query
